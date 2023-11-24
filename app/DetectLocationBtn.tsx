f4de12614523f96f89a008680822d0d6439e2ad3
krzysztofjesien <krzysztof@koii.network>
2023-11-24 19:48:30 +0100

    Use of SASS & Error handling for location detection

--- app/DetectLocationBtn.tsx
+++ app/DetectLocationBtn.tsx
@@ -1,26 +1,40 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { ValidCoordinates } from "@/app/weatherApiClient/searchLocationByQuery";
+import { useState } from "react";
 
 export default function DetectLocationBtn() {
   const router = useRouter();
-  return (
-    <button
-      type="button"
-      className="btn"
-      disabled={!navigator.geolocation}
-      onClick={() => {
-        navigator.geolocation.getCurrentPosition((detected) => {
-          const coordinates: ValidCoordinates = {
-            lat: detected.coords.latitude.toString(),
-            lon: detected.coords.longitude.toString(),
-          };
+  const [error, setError] = useState<string>();
 
-          router.push(`/weather?${new URLSearchParams(coordinates)}`);
-        });
-      }}
-    >
-      Detect
-    </button>
+  const detectLocation = () => {
+    window.navigator.geolocation.getCurrentPosition(
+      (detected) => {
+        const coordinates: ValidCoordinates = {
+          lat: detected.coords.latitude.toString(),
+          lon: detected.coords.longitude.toString(),
+        };
+
+        router.push(`/weather?${new URLSearchParams(coordinates)}`);
+      },
+      (error) => {
+        setError(error.message);
+      },
+    );
+  };
+
+  return (
+    <div className="group relative w-max">
+      <button
+        type="button"
+        className="btn"
+        disabled={!window.navigator.geolocation || Boolean(error)}
+        onClick={detectLocation}
+        title={error}
+      >
+        Detect
+      </button>
+      {error && <span className="tooltip">{error}</span>}
+    </div>
   );
 }
