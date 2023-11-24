6ed09bda132aed4ab36caa3293da9e3cfa49ccd6
krzysztofjesien <krzysztof@koii.network>
2023-11-24 03:27:22 +0100

    Initial version of displaying current weather data for provided location

--- app/weather/page.tsx
+++ app/weather/page.tsx
@@ -1,6 +1,8 @@
 import { ValidCoordinates } from "@/app/weatherApiClient/searchLocationByQuery";
 import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
+import { getCurrentWeatherData } from "@/app/weatherApiClient/getCurrentWeatherData";
+import Image from "next/image";
 
 export default async function Search({
   searchParams,
@@ -13,9 +15,34 @@ export default async function Search({
     redirect("/");
   }
 
+  const weatherData = await getCurrentWeatherData(searchParams);
+
   return (
     <main className="flex flex-col items-center justify-start p-8">
-      <div>{JSON.stringify(searchParams)}</div>
+      <div className="text-xl pb-3">{weatherData.name}</div>
+
+      <div className="flex flex-col justify-start gap-1">
+        <div>Temperature {weatherData.main.temp} °C </div>
+        <div>Felt temperature {weatherData.main.feels_like} °C</div>
+        <div>
+          {weatherData.weather.map((cond) => (
+            <div key={cond.id} className="flex flex-row items-center">
+              <Image
+                src={`https://openweathermap.org/img/wn/${cond.icon}.png`}
+                alt={cond.main}
+                width={50}
+                height={50}
+              />
+              <div>{cond.description}</div>
+            </div>
+          ))}
+        </div>
+        <div>Pressure {weatherData.main.pressure} hPa</div>
+        <div>Humidity {weatherData.main.humidity} %</div>
+        <div>Visibility {weatherData.visibility} m</div>
+        <div>Wind speed {weatherData.wind.speed} m/s</div>
+        <div>Cloudiness {weatherData.clouds.all} %</div>
+      </div>
     </main>
   );
 }
---
+++ app/weatherApiClient/getCurrentWeatherData.ts
@@ -0,0 +1,88 @@
+import { getApiKey } from "@/app/weatherApiClient/utils";
+import { ValidCoordinates } from "@/app/weatherApiClient/searchLocationByQuery";
+
+export interface CurrentWeatherDataParameters {
+  lat: string;
+  lon: string;
+  appid: string;
+  mode?: "xml" | "html"; // if undefined -> JSON
+  units?: "standard" | "metric" | "imperial";
+  lang?: string;
+}
+
+export interface CurrentWeatherDataResponse {
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  weather: [
+    {
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
+    },
+  ];
+  base: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+    sea_level: number;
+    grnd_level: number;
+  };
+  visibility: number;
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  rain: {
+    "1h": number;
+  };
+  clouds: {
+    all: number;
+  };
+  dt: number;
+  sys: {
+    type: number;
+    id: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+}
+
+export async function getCurrentWeatherData({
+  lat,
+  lon,
+}: ValidCoordinates): Promise<CurrentWeatherDataResponse> {
+  const params = {
+    appid: getApiKey(),
+    lat,
+    lon,
+    units: "metric",
+  } satisfies CurrentWeatherDataParameters;
+
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?${new URLSearchParams(
+      params,
+    )}`,
+    {
+      next: { revalidate: 30 * 60 }, //TODO Check
+    },
+  );
+
+  if (!res.ok) {
+    throw new Error("Error occurred during data fetch");
+  }
+
+  return await res.json();
+}
