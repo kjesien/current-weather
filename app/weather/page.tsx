851a0c3a869c6badbabe6b63cdeeb93de98a7cec
krzysztofjesien <krzysztof@koii.network>
2023-11-24 20:44:29 +0100

    Rename of page component after copy/pasting

--- app/weather/page.tsx
+++ app/weather/page.tsx
@@ -6,7 +6,7 @@ import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth";
 import Image from "next/image";
 
-export default async function Search({
+export default async function Weather({
   searchParams,
 }: {
   searchParams?: ValidCoordinates;
